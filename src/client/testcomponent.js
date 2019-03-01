c5a714c42cb171b2cfbe36361752aca53bcb1564
edvin palm <edpalm@outlook.com>
2019-03-01 16:04:28 +0100

    testing pm2 autorestart autocompile

--- src/client/testcomponent.js
+++ src/client/testcomponent.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <h1>autorestart relative path3!</h1>
+        <h1>autorestart relative path4!</h1>
       </div>
     )
   }
