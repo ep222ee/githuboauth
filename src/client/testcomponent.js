2a6bae448f07684a93f35a1a8d62b3daa089db5b
edvin palm <edpalm@outlook.com>
2019-03-01 15:43:28 +0100

    tested autorestart pm2 relative path

--- src/client/testcomponent.js
+++ src/client/testcomponent.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render () {
     return (
       <div>
-        <h1>Det här borde synas</h1>
+        <h1>autorestart relative path!</h1>
       </div>
     )
   }
