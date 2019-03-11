15f4533833864ef36fe3b1aa08674e8e3c2339de
edvin palm <edpalm@outlook.com>
2019-03-11 18:28:17 +0100

    reverted cookie secure

--- src/server/app.js
+++ src/server/app.js
@@ -42,7 +42,7 @@ app.use(session({
   secret: process.env.SESSION_SECRET,
   cookie: {
     httpOnly: true,
-    secure: false,
+    secure: false, // true when using ssl that's not self signed.
     maxAge: 1000 * 60 * 60 * 24,
   }
 }))
@@ -64,18 +64,7 @@ passport.deserializeUser((user, cb) => {
 // Routes.
 app.use('/', require('./routes/apiRouter.js'))
 app.use('/', require('./routes/loginRouter.js'))
-// app.use('/', require('./routes/oauthRouter.js'))
-
-// Oauth Routes
-app.get('/auth/github',
-  passport.authenticate('github', { scope: ['user', 'admin:repo', 'admin:org', 'read:org', 'admin:repo_hook'] }))
-
-app.get('/auth/github/callback',
-  passport.authenticate('github', { failureRedirect: '/' }),
-  (req, res) => {
-    res.redirect('/')
-  })
-
+app.use('/', require('./routes/oauthRouter.js'))
 
 if (process.env.NODE_ENV === 'production') {
   app.set('trust proxy', 1)
