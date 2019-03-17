0b3f95eec4b9612ce1c0dfcabd4076f377df9782
edvin palm <edpalm@outlook.com>
2019-03-17 14:31:17 +0100

    sw.js bug

--- dist/sw.js
+++ dist/sw.js
@@ -4,7 +4,6 @@ self.addEventListener('push', event => {
   let payload = event.data.json()
   console.log('push received')
   self.registration.showNotification(payload.title, {
-    console.log('show notification!')
     body: payload.body,
     icon: payload.icon
   })
