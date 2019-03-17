13eeac8a7129bb31e796100e231755e1fd673d84
edvin palm <edpalm@outlook.com>
2019-03-17 10:31:27 +0100

    no longer saves duplicate push subscribers

--- src/server/controllers/swController.js
+++ src/server/controllers/swController.js
@@ -8,23 +8,29 @@ swController.subscribeSW = async (req, res) => {
   // Setup subscription
   let subscription = req.body.subscription
   let payload = JSON.stringify({
-     title: 'Subscribed to push notifications ',
+     title: 'Push notifications active ',
      body: 'Manage which events to receive notifications for in the notification settings'
    })
   webpush.sendNotification(subscription, payload).catch(err => console.log(err))
 
+  // Check if subscription already exist
+  let checkForSubscription = await Subscriber.find({subscription: subscription})
 
-// Save subscription
-  let userID = req.user.id
-  let newSubscriber = new Subscriber({
-    userID: userID,
-    subscription: subscription
-  })
+  // Only save subscription if it doesn't exist already
+  if (checkForSubscription.length === 0) {
 
-  await newSubscriber.save((err, subscriber) => {
-    if (err) console.log(err)
-  })
-  // save subscription to db.
+    // Save subscription
+    let userID = req.user.id
+    let newSubscriber = new Subscriber({
+      userID: userID,
+      subscription: subscription
+    })
+
+    await newSubscriber.save((err, subscriber) => {
+      if (err) console.log(err)
+    })
+  }
+
   res.status(201).json({})
 }
 
